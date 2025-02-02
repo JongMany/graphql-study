5b06ab142a80e638cad8df73f5756a0a1d780835
JongMany <blackberry1114@naver.com>
2025-02-02 16:34:22 +0900

    hydration 이슈 수정

--- apps/client/components/MsgList.jsx
+++ apps/client/components/MsgList.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import MsgItem from "./MsgItem";
 
 const userIds = ["roy", "jay"];
 const getRandomUserIds = () => userIds[Math.round(Math.random())];
 
-const msgs = Array(50)
-  .fill(0)
-  .map((_, i) => ({
-    id: i + 1,
-    userId: getRandomUserIds(),
-    timestamp: 1234567890123 + i * 1000 * 60,
-    text: `${i + 1} mock text`,
-  }));
-
 function MsgList() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const msgs = Array(50)
+      .fill(0)
+      .map((_, i) => ({
+        id: i + 1,
+        userId: getRandomUserIds(),
+        timestamp: 1234567890123 + i * 1000 * 60,
+        text: `${i + 1} mock text`,
+      }));
+    setMessages(msgs);
+  }, []);
+
   return (
     <ul className="messages">
-      {msgs.reverse().map((item) => (
+      {messages.reverse().map((item) => (
         <MsgItem {...item} />
       ))}
     </ul>
